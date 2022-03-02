497e6252a7d4c047561a7ac639aa4c92b7c3c3d3
Nathan Guedes Pessoa <90534425+NathanGuedes@users.noreply.github.com>
2022-03-02 09:48:49 -0300

    atualização

--- ProjetoTcc/Pagina/HomeInstituicao.php
+++ ProjetoTcc/Pagina/HomeInstituicao.php
@@ -50,6 +50,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="PedidoRealizado.php">Pedidos Realizados</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Relatorios.php">Relatorios</a>
+                </li>
             </ul>
             <ul class="navbar-nav ml-auto nav-flex-icons">
                 <li class="nav-item dropdown">
