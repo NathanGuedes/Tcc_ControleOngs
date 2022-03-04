f0812c8eeb74589f0b475329e48e1d90321fd61f
Nathan Guedes Pessoa <90534425+NathanGuedes@users.noreply.github.com>
2022-03-04 13:07:53 -0300

    atualização

--- ProjetoTcc/Pagina/PedidoRealizado.php
+++ ProjetoTcc/Pagina/PedidoRealizado.php
@@ -1,7 +1,7 @@
 <?php
 include("connect.php");
 session_start();
-$id_inst= $_SESSION['Id_Inst'];
+$id_inst = $_SESSION['Id_Inst'];
 
 echo '
 		<table width="80%" border="1" align="center">
@@ -11,29 +11,27 @@ echo '
 		<th bgcolor="#FFFFCC" scope="col" align="center">Ver Pedidos</th>
 		
 	    </tr>';
-$par=1;
+$par = 1;
 $query = "SELECT cardapio.Id_Cardapio,cardapio.DataCardapio, cardapio.Descricao from cardapio where cardapio.fk_Instituicao_ID_Inst ='$id_inst'";
-mysqli_set_charset( $connection, 'utf8');
-if ($result=mysqli_query($connection,$query)) {
+mysqli_set_charset($connection, 'utf8');
+if ($result = mysqli_query($connection, $query)) {
     while ($dados = mysqli_fetch_assoc($result)) {
 
-        $C_Cardapio= $dados['Id_Cardapio'];
+        $C_Cardapio = $dados['Id_Cardapio'];
         $DataServ = $dados['DataCardapio'];
         $descricao = $dados['Descricao'];
 
 
 
         if ($par % 2) {
-            echo('<tr><td align="center">'.$C_Cardapio.'</td><td align="center">'.$DataServ.'</td><td align="center">'.$descricao.'</td><td align="center"><a href = "PedidoRealizado2.php?codigo='.$C_Cardapio.'">Ver Pedidos</a></td></tr>');
+            echo ('<tr><td align="center">' . $C_Cardapio . '</td><td align="center">' . $DataServ . '</td><td align="center">' . $descricao . '</td><td align="center"><a href = "PedidoRealizado2.php?codigo=' . $C_Cardapio . '">Ver Pedidos</a></td></tr>');
+        } else {
+            echo ('<tr><td align="center">' . $C_Cardapio . '</td><td align="center">' . $DataServ . '</td><td align="center">' . $descricao . '</td><td align="center"><a href = "PedidoRealizado2.php?codigo=' . $C_Cardapio . '">Ver Pedidos</a></td></tr>');
         }
-        else {
-            echo('<tr><td align="center">'.$C_Cardapio.'</td><td align="center">'.$DataServ.'</td><td align="center">'.$descricao.'</td><td align="center"><a href = "PedidoRealizado2.php?codigo='.$C_Cardapio.'">Ver Pedidos</a></td></tr>');        }
 
-        $par=$par+1;
+        $par = $par + 1;
     }
     echo '</table>';
 
     mysqli_free_result($result);
 }
-
-?>
--- ProjetoTcc/Pagina/PedidoRealizado2.php
+++ ProjetoTcc/Pagina/PedidoRealizado2.php
@@ -1,42 +1,76 @@
-<?php
-include("connect.php");
-$codCard = $_GET['codigo'];
-$quant=0;
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-echo '
-		<table width="100%" border="1" align="center">
-		<th bgcolor="#FFFFCC" scope="col" align="center">ID</th>
-		<th bgcolor="#FFFFCC" scope="col" align="center">Nome Do Solicitante</th>
-		<th bgcolor="#FFFFCC" scope="col" align="center">Telefone</th>
-		<th bgcolor="#FFFFCC" scope="col" align="center">CPF</th>
-		<th bgcolor="#FFFFCC" scope="col" align="center">Quantidade Pedida </tr>';
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>Pedidos Realizados</title>
+</head>
 
-$query = "SELECT usuario.Id_Usu, usuario.nome, usuario.CPF, usuario.Telefone,passe.Quantidade_Pedida, passe.Id_passe from passe, usuario where passe.fk_id_usuario=usuario.ID_Usu and passe.fk_Cardapio_Id_Cardapio='$codCard'";
-mysqli_set_charset( $connection, 'utf8');
-if ($result=mysqli_query($connection,$query)) {
-    while ($dados = mysqli_fetch_assoc($result)) {
+<body>
+    <center>
+        <h1>Pedidos Realizados</h1>
+    </center>
+    <br><br>
+    <?php
+    include("connect.php");
+    session_start();
+    $codCard = $_GET['codigo'];
+    $quant = 0;
 
-        $nome=$dados['nome'];
-        $cpf=$dados['CPF'];
-        $Tel=$dados['Telefone'];
-        $quant_Pedida=$dados['Quantidade_Pedida'];
-        $id_passe=$dados['Id_passe'];
-        $quant= ($quant + $quant_Pedida);
+    /*
+if ($_SESSION['login']) {
+
+echo'
+<p><strong>
+Usuario: ' .$_SESSION['nome'].' </strong></p>';
+
+} else {
+  header("location:login.php");
+}*/
+
+    ?>
+    <center>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th style="text-align: center;" scope="col">Numero do Cardapio</th>
+                    <th style="text-align: center;" scope="col">Numero do Pedido</th>
+                    <th style="text-align: center;" scope="col">Nome</th>
+                    <th style="text-align: center;" scope="col">CPF</th>
+                    <th style="text-align: center;" scope="col">Telefone</th>
+                    <th style="text-align: center;" scope="col">Quantidade Solicitada</th>
+
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+
+                $query = "SELECT usuario.Id_Usu, usuario.nome, usuario.CPF, usuario.Telefone,passe.Quantidade_Pedida, passe.Id_passe from passe, usuario where passe.fk_id_usuario=usuario.ID_Usu and passe.fk_Cardapio_Id_Cardapio='$codCard'";
+                mysqli_set_charset($connection, 'utf8');
+                if ($result = mysqli_query($connection, $query)) {
+                    while ($dados = mysqli_fetch_assoc($result)) {
+
+                        $nome = $dados['nome'];
+                        $cpf = $dados['CPF'];
+                        $Tel = $dados['Telefone'];
+                        $quant_Pedida = $dados['Quantidade_Pedida'];
+                        $id_passe = $dados['Id_passe'];
+                        $quant = ($quant + $quant_Pedida);
 
 
-            echo('<tr><td align="center">'.$id_passe.'</td><td align="center">'.$nome.'</td><td align="center">'.$Tel.'</td><td align="center">'.$cpf.'</td><td align="center">'.$quant_Pedida.'</td></tr>');
+                        echo ('<tr><td align="center">' . $codCard . '</td><td align="center">' . $id_passe . '</td><td align="center">' . $nome . '</td><td align="center">' . $cpf . '</td><td align="center">' . $Tel . '</td><td align="center">' . $quant_Pedida . '</td></tr>');
+                    }
 
-    }
-
-    mysqli_free_result($result);
-}
-echo '</table><br><br>';
-
-echo '<table width="20%" border="1" align="auto">
-    <th bgcolor="#FFFFCC" scope="col" align="center">Quantidade Total Pedida</th>
-    
-    <tr><td align="center">'.$quant.'</td></tr>
-    </table>';
-
-
-?>
+                    mysqli_free_result($result);
+                }
+                echo '<table class= "table table-striped">
+                <th style= "text-align: center;">Quantidade Total Pedida</th>
+            
+                <tr><td align="center";scope="col";>' . $quant . '</td></tr>
+                  </table>';
+                ?>
